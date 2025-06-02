add0953449523f7abe2f42fb682001d1874ef129
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-06-02 16:25:05 +0600

    adding initial server setup and get home route.

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/", (_, res) => {
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+
+export default app;
